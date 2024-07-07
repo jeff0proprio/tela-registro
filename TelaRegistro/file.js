d84664b35bc3d6d63b7cdbf3b441e6677505c76c
dogmal <jefferson280806@gmail.com>
2024-07-07 18:07:36 -0300

    ...

---
+++ TelaRegistro/file.js
@@ -0,0 +1,9 @@
+
+function Registrar(){
+
+    let LoginUsuario = document.getElementById("Nome").value
+    let EmailUsuario = document.getElementById("Email").value
+
+    alert(`Olá ${LoginUsuario} !!!\nseu Email ${EmailUsuario} foi cadastrado com sucesso`)
+
+}
